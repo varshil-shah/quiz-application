79c910cea7c0cca4aa4a0455579c75fe8d20c588
Varshil-Shah <varshilashah09@gmail.com>
2021-09-29 20:29:00 +0530

    Handle select-unselect of categories

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const startButton = document.querySelector(".start_btn");
 const availableCategories = document.querySelector(".wrapper");
 const displayCatogories = document.querySelector("#available_tags");
+const categoryProceedButton = document.querySelector(".proceed");
+const selectedCategories = [];
+
+let remainingCategories = document.querySelector("#remainingCategories");
+
+// Init
+categoryProceedButton.disabled = true;
+categoryProceedButton.style.backgroundColor = "#e2e2e2";
 
 const categoriesUrl = "https://opentdb.com/api_category.php";
 async function displayAvailableCategories() {
@@ -20,4 +28,31 @@ const setCategories = function (data) {
   displayCatogories.insertAdjacentHTML("afterbegin", htmlCode);
 };
 
+const handleCategories = function (e) {
+  e.preventDefault();
+  const currentCategory = e.target;
+  const categoryId = currentCategory.dataset.categoryId;
+
+  if (currentCategory.classList.contains("tag_selected")) {
+    currentCategory.classList.remove("tag_selected");
+    selectedCategories.pop(categoryId);
+  } else if (currentCategory.classList.contains("tag")) {
+    if (selectedCategories.length < 5) {
+      currentCategory.classList.add("tag_selected");
+      selectedCategories.push(categoryId);
+    }
+  }
+
+  if (selectedCategories.length > 0) {
+    categoryProceedButton.disabled = false;
+    categoryProceedButton.style.backgroundColor = "crimson";
+  } else {
+    categoryProceedButton.disabled = true;
+    categoryProceedButton.style.backgroundColor = "#e2e2e2";
+  }
+
+  remainingCategories.textContent = 5 - selectedCategories.length;
+};
+
 startButton.addEventListener("click", displayAvailableCategories);
+displayCatogories.addEventListener("click", handleCategories);
