ead908f5646497740a3be7011b3ff96953e64f88
Varshil-Shah <varshilashah09@gmail.com>
2021-09-30 11:44:00 +0530

    Display question according to selected category

--- script.js
+++ script.js
@@ -10,11 +10,32 @@ const informationBoxContinue = document.querySelector(
 const informationPage = document.querySelector(".info_box");
 const selectedCategories = [];
 
+// Quiz container-
+const question = document.querySelector(".que_text");
+const optionsList = document.querySelector(".option_list");
+const optionsArray = document.querySelectorAll(".option");
+console.log(optionsArray);
+
+const currQuestionData = {
+  currentQuestion: 0,
+  questionLimit: 15,
+  timer: [30, 45, 60],
+  levels: ["easy", "medium", "hard"],
+};
+
 let remainingCategories = document.querySelector("#remainingCategories");
 
 // Init
 categoryProceedButton.disabled = true;
 categoryProceedButton.style.backgroundColor = "#e1e1e1";
+
+/*
+  randomNumber function generates a random number,
+  returns a random value from an array,
+*/
+const randomNumber = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 const categoriesUrl = "https://opentdb.com/api_category.php";
 async function displayAvailableCategories() {
@@ -39,7 +60,7 @@ const handleCategories = function (e) {
   const currentCategory = e.target;
   const categoryId = currentCategory.dataset.categoryId;
 
-  /** 
+  /* 
     IF CONDITON-
     If the user clicks the category which is already been selected, then it will deselect the 
     category also will remove the category id from the array.
@@ -58,7 +79,7 @@ const handleCategories = function (e) {
     }
   }
 
-  /**
+  /*
     If any of the category is selected the proceed button will be activated, else it will be 
     disabled
   */
@@ -80,9 +101,47 @@ const displayInfoPage = () => {
   startButton.style.display = "none";
 };
 
-const displayQuizContainer = () => {
-  informationPage.style.display = "none";
+async function fetchQuiz() {
+  const randomCategoryId = randomNumber(selectedCategories);
+  const response = await fetch(
+    `https://opentdb.com/api.php?amount=15&category=${randomCategoryId}&difficulty=medium&type=multiple`
+  );
+  console.log(selectedCategories.length, randomCategoryId);
+  const data = await response.json();
   quizContainer.style.display = "block";
+  informationPage.style.display = "none";
+  setDynamicQuestions(data);
+}
+
+/*
+  Shuffle array will shuffle elements from an array,
+  For example: [1,2,3,4,5], array.length = 5,
+  j will have a random number between 0 - 4,
+  If any number repeats, it's index will be changed again,
+*/
+const shuffleArray = (array, element) => {
+  array.push(element);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    console.log(`j: ${j} | i: ${i}`);
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+// Set dynamic questions-
+const setDynamicQuestions = (data) => {
+  const newQuestion = data.results[0].question;
+  question.innerHTML = `<span>${newQuestion}</span>`;
+  const availableOptions = shuffleArray(
+    data.results[0].incorrect_answers,
+    data.results[0].correct_answer
+  );
+  optionsArray.forEach((element, i) => {
+    element.innerHTML = `<span>${availableOptions[i]}</span>`;
+  });
 };
 
 // Event handlers
@@ -90,4 +149,4 @@ startButton.addEventListener("click", displayInfoPage);
 displayCatogories.addEventListener("click", handleCategories);
 informationBoxExit.addEventListener("click", reloadPage);
 informationBoxContinue.addEventListener("click", displayAvailableCategories);
-categoryProceedButton.addEventListener("click", displayQuizContainer);
+categoryProceedButton.addEventListener("click", fetchQuiz);
